f13918285c88644177776558a795d10eac4603ed
zhidehuiweii <zhidehuiweii@163.com>
2019-03-11 17:49:07 +0800

    metro

--- src/components/find.vue
+++ src/components/find.vue
@@ -1,5 +1,12 @@
 <template>
+  <div id="app">
+    <div class="top_txt">
+      <span>发现</span>
+    </div>
+    <div class="top_swiper">
 
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +15,17 @@
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+  @rem: 750/10rem;
+  body{
+    background-color: #fff;
+  }
+.top_txt{
+  margin-top: 40/@rem;
+  span{
+    margin: 0 0 0 35/@rem;
+    font-size: 48/@rem;
+    line-height: 48/@rem;
+  }
+}
 </style>
